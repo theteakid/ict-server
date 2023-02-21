aed9d571cc24141d660cc4aba886008fcd2bf654
HyperBox <73667233+HyperBOXed@users.noreply.github.com>
2023-02-21 20:07:46 +0400

    main page update 3

---
+++ main/index.js
@@ -0,0 +1,6 @@
+document.querySelector('.contact-block .cross-close').addEventListener('click',()=>{
+    document.querySelector('.contact-block').remove();
+});
+document.querySelector('.nav__search .nav__search-ico').addEventListener('click',()=>{
+    document.querySelector('.nav__search input').focus();
+});
