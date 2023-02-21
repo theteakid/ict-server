a6ee134a7ee8e822959d3401945c59c9621c7b44
HyperBox <73667233+HyperBOXed@users.noreply.github.com>
2023-02-21 22:36:29 +0400

    main page update 4 + added dropdown

---
+++ main/dropdown.js
@@ -0,0 +1,27 @@
+var dd = document.querySelectorAll(".dropdown-container");
+dd.forEach(list => {
+    for (var i=0; i<list.querySelectorAll(".dropdown .cont .option").length; i++) {
+        if (list.querySelector(".dropdown .upper").getAttribute("chosenOption")==i) {
+            list.querySelector(".dropdown .upper").setAttribute("chosenOption",i);
+            list.querySelector(".dropdown .upper p").innerHTML=list.querySelectorAll(".dropdown .cont .option")[i].innerText;
+            list.querySelector(".dropdown .upper #ico-lang").style=`background-image: url('${list.querySelectorAll('.dropdown .cont .option img')[i].getAttribute('src')}');`;
+        }
+    }
+});
+/*window.addEventListener("click",(ev)=>{
+    var dd = document.querySelectorAll(".dropdown-container");
+    dd.forEach(list => {
+        if (ev.target!==list.querySelector(".dropdown .upper")&&ev.target!==list.querySelector(".dropdown .upper p")) {
+            list.querySelector(".dropdown .cont").classList.add("hidden");
+            list.querySelectorAll(".dropdown .cont .option").forEach(element => {
+                if (ev.target==element) {
+                    list.querySelector(".dropdown .upper").setAttribute("chosenOption",element.getAttribute("id"));
+                    list.querySelector(".dropdown .upper p").innerHTML=element.innerText;
+                    list.querySelector(".dropdown .upper #ico-lang").style=`background-image: url('${element.querySelector('img').getAttribute('src')}');`
+                }
+            });
+        }else{
+            list.querySelector(".dropdown .cont").classList.toggle("hidden");
+        }
+    });
+});*/
